1994673f89f005e96d1b7909c7616c348857b16c
carlosantana07 <carlosantana07@hotmail.fr>
2020-06-25 02:49:58 -0400

    j

--- index.js
+++ index.js
@@ -1,32 +1,78 @@
-//I defined two headers, one for text/html, and the other for text/css
-var commonHeaders = {'Content-Type': 'text/html'};
-var cssHeaders = {'Content-Type': 'text/css'};
-
-function css(request, response) {
-    response.writeHead(200, {'Content-type' : 'text/css'});
-    var fileContents = fs.readFileSync('./css/styles.css', {encoding: 'utf8'});
-    response.write(fileContents);
-  
-} 
-
-const http = require('http');
-
-const fs = require('fs');
-
-const server = http.createServer((request, response) => {
-    response.writeHead(200, commonHeaders);
-    fs.readFile('./Acceuil.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-});
 
 const port = process.env.PORT || 1337;
-server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+
+// HTTP Module for Creating Server and Serving Static Files Using Node.js
+// Static Files: HTML, CSS, JS, Images
+// Get Complete Source Code from Pabbly.com
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function(req, res){
+    
+
+    if(req.url === "/"){
+        fs.readFile("./acceuil.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, './', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else if(req.url.match("\.html$")){
+        var htmlPath = path.join(__dirname, './', req.url);
+        var fileStream = fs.createReadStream(htmlPath);
+        res.writeHead(200, {"Content-Type": "text/html"});
+        fileStream.pipe(res);
+    }else if(req.url.match("\.js$")){
+        var jsPath = path.join(__dirname, './', req.url);
+        var fileStream = fs.createReadStream(jsPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/js"});
+        fileStream.pipe(res);
+
+    }else if(req.url.match("\.png$")){
+        var imagePath = path.join(__dirname, './', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "image/png"});
+        fileStream.pipe(res);
+    }else if(req.url.match("\.jpg")){
+        var imagePath = path.join(__dirname, './', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "image/jpg"});
+        fileStream.pipe(res);
+    }else if(req.url.match("\.ico$")){
+        var imagePath = path.join(__dirname, './', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "icon/ico"});
+        fileStream.pipe(res);
+    }else if(req.url.match("\.woff$")){
+        var fontPath = path.join('./fonts/fontawesome-webfont.woff');
+        var fileStream = fs.createReadStream(fontPath);
+        res.writeHead(200, {"Content-Type": "application/font-woff"});
+        console.log(fontPath);
+        fileStream.pipe(res);
+    }else if(req.url.match("\.woff2$")){
+        var fontPath = path.join('./fonts/fontawesome-webfont.woff2');
+        var fileStream = fs.createReadStream(fontPath);
+        res.writeHead(200, {"Content-Type": "application/font-woff2"});
+        console.log("font2: "+fontPath);
+        fileStream.pipe(res);
+    }else if(req.url.match("\.ttf$")){
+        var fontPath = path.join('./', req.url);
+        var fileStream = fs.createReadStream(fontPath);
+        res.writeHead(200, {"Content-Type": "application/font-woff2"});
+        console.log("font2: "+fontPath);
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        
+    res.end("Ressources: "+ req.url);
+    }
+
+}).listen(port);
+
