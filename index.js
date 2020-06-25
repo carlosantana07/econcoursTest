f0eeedf97e528c2785ac9a28be29027f59b8497f
carlosantana07 <carlosantana07@hotmail.fr>
2020-06-25 02:50:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,18 +45,13 @@ http.createServer(function(req, res){
         var fileStream = fs.createReadStream(imagePath);
         res.writeHead(200, {"Content-Type": "image/jpg"});
         fileStream.pipe(res);
-    }else if(req.url.match("\.ico$")){
-        var imagePath = path.join(__dirname, './', req.url);
-        var fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, {"Content-Type": "icon/ico"});
-        fileStream.pipe(res);
-    }else if(req.url.match("\.woff$")){
-        var fontPath = path.join('./fonts/fontawesome-webfont.woff');
+    }else if(req.url.match("\.woff?v=4.4.0$")){
+        var fontPath = path.join(__dirname, './', "fonts/fontawesome-webfont.woff");
         var fileStream = fs.createReadStream(fontPath);
         res.writeHead(200, {"Content-Type": "application/font-woff"});
         console.log(fontPath);
         fileStream.pipe(res);
-    }else if(req.url.match("\.woff2$")){
+    }else if(req.url.match("\.woff2?v=4.4.0$")){
         var fontPath = path.join('./fonts/fontawesome-webfont.woff2');
         var fileStream = fs.createReadStream(fontPath);
         res.writeHead(200, {"Content-Type": "application/font-woff2"});
@@ -76,3 +71,92 @@ http.createServer(function(req, res){
 
 }).listen(port);
 
+/*const http = require("http"),
+      fs = require("fs"),
+      path = require("path"),
+      log = console.log;
+
+//this will response with file type with specifiq content and encode
+function response(res,file,contentType,encode){
+    if(file != "404"){
+        res.writeHead(200,{"Content-type":contentType});
+        if(encode){
+            var stream = fs.createReadStream(path.join(__dirname,file),encode);
+        }else{
+            var stream = fs.createReadStream(path.join(__dirname,file));
+        }
+        stream.pipe(res);
+    }else{
+        res.writeHead(404,{"Content-type":"text/html"});
+        res.end(`
+            ${contentType}
+        `);
+    }
+}
+//response as text 
+function responseText(res,file,contentType){ response(res,file,contentType,"utf-8"); }
+//response as binary
+function responseBinary(res,file,contentType){response(res,file,contentType); }
+
+//check the filetype and respose as the type..
+function checkReqAndSend(req,res){
+    if(req.match(/\.css$/)){
+        responseText(res,req,"text/css");
+    }else if(req.match(/\.js$/)){
+        responseText(res,req,"text/js");
+    }else if(req.match(/\.json$/)){
+        responseText(res,req,"application/json");
+    }else if(req.match(/\.html$/)){
+        responseText(res,req,"text/html");
+    }else if(req.match(/\.svg$/)){ //svg Binary file
+        responseBinary(res,req,"image/svg+xml");
+    }else if(req.match(/\.woff2$/)){
+        responseBinary('/fonts/fontawesome-webfont.woff2','/fonts/fontawesome-webfont.woff2',"application/font-woff2");
+    }else if(req.match(/\.woff$/)){
+        responseBinary(res,req,"application/font-woff");
+    }else if(req.match(/\.eot$/)){
+        responseBinary(res,req,"text/vnd.ms-fontobject eot");
+    }else if(req.match(/\.ttf$/)){
+        responseBinary(res,req,"text/octet-stream");
+    }else if(req.match(/\.map$/)){ //done
+        responseBinary(res,req,"text/octet-stream");
+    }else if(req.match(/\.jpg$/)){ 
+        responseBinary(res,req,"image/jpg");
+    }else if(req.match(/\.png$/)){
+        responseBinary(res,req,"image/png");
+    }else if(req.match(/\.jpeg$/)){
+        responseBinary(res,req,"image/jpeg");
+    }else if(req.match(/^\/[a-zA-Z]{1}[a-zA-Z0-9]{2,8}$/)){
+        var file = path.join(__dirname,"public",req)+".html";
+        fs.access(file,function(err){
+            if(err){
+                 response(res,"404",
+                        `<h1 style="color:green; font-size:5em; font-weight:100;">
+                        Ok Successful route But We Don't find the file...
+                    </h1>`);
+            }else{
+                responseText(res,`${req}.html`,"text/html");
+            }
+        });
+    }else{
+        response(res,"404",res.url);
+    }
+}
+
+
+//make the server...
+http.createServer(function(req,res){
+    log(req.url);
+    if(req.url === "/"){
+        res.writeHead(200,{"Content-type":"text/html"});
+        fs.readFile(path.join(__dirname,"acceuil.html"),function(err,data){
+            err ? log("Error sending html") : res.end(data);
+        });
+    }else{
+        checkReqAndSend(req.url,res);
+    }
+}).listen(port,function(err){
+    if(!err){log("server listen on localhost:3000");}
+    else{log("Error to create server");}
+});
+*/
