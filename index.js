622c8ff869065587c8884d6c140e631d4348a6b9
carlosantana07 <carlosantana07@hotmail.fr>
2020-06-26 00:57:53 -0400

    .

--- index.js
+++ index.js
@@ -2,76 +2,24 @@
 const port = process.env.PORT || 1337;
 
 
-// HTTP Module for Creating Server and Serving Static Files Using Node.js
-// Static Files: HTML, CSS, JS, Images
-// Get Complete Source Code from Pabbly.com
-
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-
-require('dotenv').config();
-console.log(process.env);
-
 var SERVER_URL_PREFIX; 
 
-http.createServer(function(req, res){
-    SERVER_URL_PREFIX  = process.env.SERVER_URL_PREFIX; 
 
-    if(req.url === "/"){
-        fs.readFile("./acceuil.html", "UTF-8", function(err, html){
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(html);
-        });
-    }else if(req.url.match("\.css$")){
-        var cssPath = path.join(__dirname, './', req.url);
-        var fileStream = fs.createReadStream(cssPath, "UTF-8");
-        res.writeHead(200, {"Content-Type": "text/css"});
-        fileStream.pipe(res);
+const express = require('express');
+const app = express();
 
-    }else if(req.url.match("\.html$")){
-        var htmlPath = path.join(__dirname, './', req.url);
-        var fileStream = fs.createReadStream(htmlPath);
-        res.writeHead(200, {"Content-Type": "text/html"});
-        fileStream.pipe(res);
-    }else if(req.url.match("\.js$")){
-        var jsPath = path.join(__dirname, './', req.url);
-        var fileStream = fs.createReadStream(jsPath, "UTF-8");
-        res.writeHead(200, {"Content-Type": "text/js"});
-        fileStream.pipe(res);
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-    }else if(req.url.match("\.png$")){
-        var imagePath = path.join(__dirname, './', req.url);
-        var fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, {"Content-Type": "image/png"});
-        fileStream.pipe(res);
-    }else if(req.url.match("\.jpg")){
-        var imagePath = path.join(__dirname, './', req.url);
-        var fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, {"Content-Type": "image/jpg"});
-        fileStream.pipe(res);
-    }else if(req.url.match("\.woff2$")){
-        var fontPath = path.join(__dirname, './', "fonts/fontawesome-webfont.woff");
-        var fileStream = fs.createReadStream(fontPath);
-        res.writeHead(200, {"Content-Type": "application/font-woff2"});
-        console.log(fontPath);
-        fileStream.pipe(res);
-    }else if(req.url.match("\.woff$")){
-        var fontPath = path.join('./fonts/fontawesome-webfont.woff');
-        var fileStream = fs.createReadStream(fontPath);
-        res.writeHead(200, {"Content-Type": "application/font-woff"});
-        console.log("font2: "+fontPath);
-        fileStream.pipe(res);
-    }else if(req.url.match("\.ttf$")){
-        var fontPath = path.join('./', req.url);
-        var fileStream = fs.createReadStream(fontPath);
-        res.writeHead(200, {"Content-Type": "application/font-woff2"});
-        console.log("font2: "+fontPath);
-        fileStream.pipe(res);
-    }else{
-        res.writeHead(404, {"Content-Type": "text/html"});
-        
-    res.end("Ressources: "+ req.url);
-    }
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('./'));
 
-}).listen(port);
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + './Acceuil.html');
+});
+
+// listen for requests :)
+const listener = app.listen(port, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
